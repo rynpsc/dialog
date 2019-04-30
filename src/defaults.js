8f05bb23c68bf3ded65cbf9b145f1f299b44205c
Ryan Pascoe <ryan.pascoe@outlook.com>
2019-04-30 20:54:11 +0100

    Add docblock to default configuration object

--- src/defaults.js
+++ src/defaults.js
@@ -1,30 +1,16 @@
+/**
+ * Default configuration.
+ *
+ * @property {string} label - ID of HTMLElement to use as label or a string.
+ * @property {string} description - ID of HTMLElement to use as aria-describedby.
+ * @property {boolean} alert - Set dialog role to alertdialog.
+ * @property {string} openClass - The class applied to elements.dialog when open.
+ */
 const defaults = {
-	/**
-	 * Label
-	 */
 	label: 'Dialog',
-
-	/**
-	 * Description - ID of HTMLElement to use as aria-describedby
-	 */
 	description: '',
-
-	/**
-	 * Focus - HTMLElement to focus on open
-	 */
-	focus: '',
-
-	/**
-	 * Alert - Is the dialog an alert?
-	 */
 	alert: false,
-
 	openClass: 'is-open',
-
-	/**
-	 * Auto initiate on instantiation
-	 */
-	autoInit: true,
 };
 
 export default defaults;
