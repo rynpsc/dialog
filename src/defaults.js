cfeaa7699a5391b537b129079fec7a011aa82e42
Ryan Pascoe <ryan.pascoe@outlook.com>
2019-06-03 07:43:59 +0100

    Better comments for default values

--- src/defaults.js
+++ src/defaults.js
@@ -1,8 +1,8 @@
 /**
  * Default configuration.
  *
- * @property {string} label - ID of HTMLElement to use as label or a string.
- * @property {string} description - ID of HTMLElement to use as aria-describedby.
+ * @property {string} label - ID of HTMLElement to use in aria-labelledby or a string to use as the aria-label.
+ * @property {string} description - ID of HTMLElement to use in aria-describedby.
  * @property {boolean} alert - Set dialog role to alertdialog.
  * @property {string} openClass - The class applied to elements.dialog when open.
  */
